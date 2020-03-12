f7089937443f466bace6fd8ed9d5d422f47d84b5
nicholas-russell <nrussell97@outlook.com>
2020-03-12 13:36:54 +1100

    Added localisations context processor

---
+++ fco_database/localisations.py
@@ -0,0 +1,10 @@
+def template_vars(request):
+    return {
+        "new_member": {
+            "volunteer_text": "Select any volunteering you would be interested in by searching",
+            "mailing_list_text": "Please tick if you <strong>do not</strong> want to be included in our \
+                                mailing list. The Food Co-operative is reliant on volunteers to be able to \
+                                give greater benefits back to our members and community. If you opt out of our \
+                                mailing list, you’ll miss out on emails regarding volunteering opportunities."
+        }
+    }
